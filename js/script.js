fd8af8e3b7ac58a0c36104f0d7ac74e090864e20
lankoni7 <bandarmirza@gmail.com>
2021-10-04 18:39:57 +0700

    Slider is done

--- js/script.js
+++ js/script.js
@@ -5,3 +5,35 @@ hamburger.addEventListener("click", function () {
   menu.classList.toggle("menu_full-screen");
   hamburger.classList.toggle("hamburger_close");
 });
+
+const leftBtn = document.querySelector(".arrow_left");
+const rightBtn = document.querySelector(".arrow_right");
+const slider = document.querySelector(".slider");
+const slide = document.querySelector(".slide");
+const slides = document.querySelectorAll(".slide");
+const slideWidth = window.getComputedStyle(slides[0]).width;
+console.log(slideWidth);
+
+const step = parseFloat(slideWidth);
+const minRight = 0;
+const maxRight = (slides.length - 1) * step;
+console.log(maxRight);
+let currentRight = 0;
+
+slider.style.right = currentRight;
+
+rightBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentRight < maxRight) {
+    currentRight += step;
+    slider.style.right = `${currentRight}px`;
+  }
+});
+
+leftBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentRight > minRight) {
+    currentRight -= step;
+    slider.style.right = `${currentRight}px`;
+  }
+});
